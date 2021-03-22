2fcd47824bcc7abf52ba8e6ef6913f03301e1f79
dineshdevkota001 <dineshdevkota001@gmail.com>
2021-03-22 17:51:07 +0545

    UI first commit

---
+++ js/game.js
@@ -0,0 +1,36 @@
+$(document).ready(() => {
+    // initiate json
+    // initiate reflectors
+    // initiate event handlers
+    game();
+})
+
+const game = () =>{
+    // initiate variables
+
+    // game loop
+
+        // get type
+
+        // display available modes
+
+        // check health
+
+        // change time
+
+}
+
+// anti cheat
+// compare the previous change to this one. if the change is greater than 5 then it is probably cheating
+
+
+// parser
+const input = () =>{
+    // check enter
+    // get changes
+    // return changes
+}
+
+const selection = () =>{
+    // selection thingy
+}
