c3d1c6a0db318238163c13fdf416bf1e431349b5
dineshdevkota001 <dineshdevkota001@gmail.com>
2021-03-24 02:23:35 +0545

    proper spells added

--- assets/attack.js
+++ assets/attack.js
@@ -1,11 +1,11 @@
 const attack =[
-    {cast:"fire",time:1,damage:70,mp:100},
-    {cast:"some fire",time:5,damage:10,mp:100},
-    {cast:"xforce",time:5,damage:10,mp:300},
-    {cast:"fire",time:1,damage:70,mp:100},
-    {cast:"some fire",time:5,damage:10,mp:100},
-    {cast:"xforce",time:5,damage:10,mp:300},
-    {cast:"fire",time:1,damage:70,mp:100},
-    {cast:"some fire",time:5,damage:10,mp:100},
-    {cast:"xforce",time:5,damage:10,mp:300}
+    {cast:"fire",time:1,damage:30,mp:100},
+    {cast:"raise from the bottom of hell and shave this puny life",time:4,damage:400,mp:400},
+    {cast:"explosionnn",time:1,damage:70,mp:200},
+    {cast:"random spirit of this world heed my call and bring forth destruction",time:5,damage:400,mp:300},
+    {cast:"die by the flame live by the flame survive by the flame grown by the flame i am the child of the flame now heed my call explosion",time:10,damage:1000,mp:500},
+    {cast:"firebolt",time:0.5,damage:60,mp:80},
+    {cast:"ice prison",time:1,damage:50,mp:100},
+    {cast:"feast upon this blood suck him dry coffin of the sand",time:7,damage:300,mp:300},
+    {cast:"go go power rangers",time:3,damage:15,mp:200}
 ];
